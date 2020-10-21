87952f48a7497aa2978a20310830b7cf360cc40d
rafael <rafaelcmd@gmail.com>
2020-10-20 21:22:46 -0300

    Changed test

--- go/src/main_test.go
+++ go/src/main_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestGreeting(t *testing.T) {
 	greeting := greeting("Code.education Rocks!")
-	if greeting != "Code.education Rocks!" {
+	if greeting != "<b>Code.education Rocks!</b>" {
 		t.Error("O texto não é: ", greeting)
 	}
 }
